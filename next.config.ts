f634c8c21c4b5165c4cef4fb77172bdad1f795a9
Jaskirat Gill <gilljaskirat04@gmail.com>
2025-03-31 19:10:39 -0700

    update next config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["bella-artistry-admin.jaskiratgill.ca"], // Add your external image domain here
+    domains: ["admin.bellaartistry.ca"], // Add your external image domain here
   },
 };
 
